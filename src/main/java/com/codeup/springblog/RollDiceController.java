b780c3f521cef16132b11f463649e346e5f96f06
Karla Jara <jara.karla.43@gmail.com>
2021-06-11 10:11:22 -0500

    added files for partials dir.

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class RollDiceController {
 
-    @GetMapping("/roll-dice/{num}")
-    public String getGuess(@PathVariable int num, Model model) {
+    @GetMapping("/roll-dice/{num} {num2}")
+    public String getGuess(@PathVariable int num, int num2, Model model) {
         model.addAttribute("userGuess", num);
+        model.addAttribute("userGuess", num2);
         model.addAttribute("hasGuessed", true);
         int n = 1 + (int)(Math.random() * (6 - 1));
+        int n2 = 1 + (int)(Math.random() * (6 - 1));
         model.addAttribute("guess", n);
+        model.addAttribute("guess", n2);
         return "roll-dice";
     }
 
